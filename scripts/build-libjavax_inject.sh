93e1a851be19fba3faf5d062d6d7fd933e59ef87
Joe Charlier <joe@Babakinidae.local>
2022-04-13 14:25:12 +0900

    adjustments to scripts

--- scripts/build-libjavax_inject.sh
+++ scripts/build-libjavax_inject.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-rm -rf ../MoboJava/Frameworks/libjavax_inject.xcframework
+rm -rf ../libjavax_inject.xcframework
 
 mkdir ios-arm64
 mkdir ios-arm64_x86_64-simulator
 mkdir macos-arm64_x86_64
 
-cp ~/j2objc/inject/javax_inject/build_result/objs-iphone64/libjavax_inject.a ios-arm64
+cp ../j2objc/inject/javax_inject/build_result/objs-iphone64/libjavax_inject.a ios-arm64
 
 lipo -create \
-	~/j2objc/inject/javax_inject/build_result/objs-simulator/libjavax_inject.a \
-	~/j2objc/inject/javax_inject/build_result/objs-simulator64/libjavax_inject.a \
+	../j2objc/inject/javax_inject/build_result/objs-simulator/libjavax_inject.a \
+	../j2objc/inject/javax_inject/build_result/objs-simulator64/libjavax_inject.a \
 	-output ios-arm64_x86_64-simulator/libjavax_inject.a
 
 lipo -create \
-	~/j2objc/inject/javax_inject/build_result/objs-macosx/libjavax_inject.a \
-	~/j2objc/inject/javax_inject/build_result/objs-macosx64/libjavax_inject.a \
+	../j2objc/inject/javax_inject/build_result/objs-macosx/libjavax_inject.a \
+	../j2objc/inject/javax_inject/build_result/objs-macosx64/libjavax_inject.a \
 	-output macos-arm64_x86_64/libjavax_inject.a
 
 xcodebuild -create-xcframework \
 	-library ios-arm64/libjavax_inject.a -headers ~/j2objc/inject/javax_inject/build_result/JavaxInject.h \
 	-library ios-arm64_x86_64-simulator/libjavax_inject.a -headers ~/j2objc/inject/javax_inject/build_result/JavaxInject.h \
 	-library macos-arm64_x86_64/libjavax_inject.a -headers ~/j2objc/inject/javax_inject/build_result/JavaxInject.h \
-	-output ../MoboJava/Frameworks/libjavax_inject.xcframework
+	-output ../libjavax_inject.xcframework
 
 rm -rf ios-arm64
 rm -rf ios-arm64_x86_64-simulator
---
+++ scripts/make-j2objc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#	iphone64:	iOS arm64,
+#	simulator:	Simulator x86_64,
+#	simulator64:	Simulator arm64
+#	macosx:		macOS x86_64 	
+#	macosx64:	macOS arm64
+ 
+export J2OBJC_ARCHS="iphone64 simulator simulator64 macosx macosx64"
+cd ../j2objc
+make -j8 frameworks
