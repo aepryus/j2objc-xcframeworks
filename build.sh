2994a16c98e109f677b1949fe9909453ebc9f158
Joe Charlier <joe@aepryus.com>
2022-04-13 20:51:16 +0900

    more shuffling

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 #	macosx:         macOS x86_64 	
 #	macosx64:       macOS arm64
  
-cp Resources/common.mk j2objc/make
+cp common.mk j2objc/make
 export J2OBJC_ARCHS="iphone64 simulator simulator64 macosx macosx64"
 cd j2objc
 make -j8 frameworks
