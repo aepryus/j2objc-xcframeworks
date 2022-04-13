e180bb601b9d38c9d1f093853c04a1cd42a9a1e9
Joe Charlier <joe@aepryus.com>
2022-04-13 18:34:48 +0900

    hacked fix for -Wunused-but-set-variable warning

--- scripts/make-j2objc.sh
+++ scripts/make-j2objc.sh
@@ -6,6 +6,8 @@
 #	macosx:		macOS x86_64 	
 #	macosx64:	macOS arm64
  
+cp Resources/common.mk ../j2objc/make
+cp Resrouces/j2objcc.sh ../j2objc/scripts
 export J2OBJC_ARCHS="iphone64 simulator simulator64 macosx macosx64"
 cd ../j2objc
 make -j8 frameworks
